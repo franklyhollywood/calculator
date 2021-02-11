6078c733a508f95fcb476776c3b3610552f9869f
franklyhollywood <github@srvsync.com>
2021-02-11 11:11:41 -0800

    added calculator(s)

--- app.js
+++ app.js
@@ -1,5 +1,60 @@
-// import functions and grab DOM elements
+// grab dom elements
 
-// initialize state
+// set event listeners on each button, do math, and update DOM
 
-// set event listeners to update state and DOM
+const num1 = document.getElementById('input1');
+const num2 = document.getElementById('input2');
+
+const btn = document.getElementById('button-add');
+const result = document.getElementById('result1');
+
+
+btn.addEventListener('click', () => {
+    result.textContent = Number(num1.value) + Number(num2.value);
+    //num1.value = '';
+    //num2.value = '';
+});
+
+
+
+const num3 = document.getElementById('input3');
+const num4 = document.getElementById('input4');
+
+const btn2 = document.getElementById('button-subtract');
+const result2 = document.getElementById('result2');
+
+
+btn2.addEventListener('click', () => {
+    result2.textContent = Number(num3.value) - Number(num4.value);
+    //num1.value = '';
+    //num2.value = '';
+});
+
+
+const num7 = document.getElementById('input7');
+const num8 = document.getElementById('input8');
+
+const btn4 = document.getElementById('button-divide');
+const result4 = document.getElementById('result4');
+
+
+btn4.addEventListener('click', () => {
+    result4.textContent = Number(num7.value) / Number(num8.value);
+    //num1.value = '';
+    //num2.value = '';
+});
+
+
+
+const num5 = document.getElementById('input5');
+const num6 = document.getElementById('input6');
+
+const btn3 = document.getElementById('button-multiply');
+const result3 = document.getElementById('result3');
+
+
+btn3.addEventListener('click', () => {
+    result3.textContent = Number(num5.value) * Number(num6.value);
+    //num1.value = '';
+    //num2.value = '';
+});
